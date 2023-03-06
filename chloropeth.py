10ae71b52031311e43ab373d043e8841180a6c31
Tala Suidan <talamsuidan@gmail.com>
2023-03-06 12:57:16 -0500

    add chloropeth

--- chloropeth.py
+++ chloropeth.py
@@ -6,8 +6,66 @@ Created on Sun Mar  5 23:31:37 2023
 """
 import json
 import pandas as pd
+import numpy as np
+import descartes
+import matplotlib as plt
+import geopandas as gpd
+import plotly_express as px
+import requests
 
 
-ga_json='./data/5m-US-counties.json'
-with open(ga_json,'r') as f:
-    data=json.load(f)
+# ga_json='./data/5m-US-counties.json'
+# with open(ga_json,'r') as f:
+#     data=json.load(f.text)
+    
+r = requests.get(
+    'https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json'
+    )
+counties = json.loads(r.text)
+target_states = ["13"]
+al = [i for i in counties["features"] if i["properties"]["STATE"] in target_states]
+df2=pd.json_normalize(al, max_level=1)
+df2=df2.set_index('properties.NAME')
+df2=df2['id']
+
+
+for dataset in [ '2018-19','2019-20','2020-21']:
+    
+    
+    if dataset=='2018-19':
+        title_1='2018-19'
+        
+        
+    if dataset=='2019-20':
+         
+         title_1='2019-20'    
+        
+    if dataset=='2020-21':
+         
+                
+          title_1='2020-21'
+          
+          
+            
+
+    map_dict=dict(df2)
+    
+    df = pd.read_csv('./csvs/{}_with labels.csv'.format(title_1))
+    df['SCHOOL_DSTRCT_NM']=df['SCHOOL_DSTRCT_NM'].replace({' County':''},regex=True)
+    df['SCHOOL_DSTRCT_NM']=df['SCHOOL_DSTRCT_NM'].replace({' Public Schools':''},regex=True)
+    df['FIPS']=df.SCHOOL_DSTRCT_NM.map(map_dict)
+
+
+
+
+
+    fig = px.choropleth(df, geojson=counties, locations='FIPS', color='Labels',
+                        color_continuous_scale='Viridis',
+                        range_color=(0, 14),
+                        scope='usa',
+                        labels={'Labels': 'Performance'}
+                        )
+    fig.update_geos(fitbounds='locations', visible=False)
+    fig.update_layout(margin={'r': 0, 't': 0, 'l': 0, 'b': 0},
+                      title={'text': '{} School Year'.format(title_1),  'y':0.9, 'x':0.5,'xanchor': 'center','yanchor': 'top' })
+    fig.write_image('./images/Chloropeth_{}.png'.format(title_1))
