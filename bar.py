899ad72be45c1c6e3b5cdedc9cc20ff96fd4be0d
Tala Suidan <talamsuidan@gmail.com>
2023-03-14 12:23:14 -0400

    add bar plots

--- bar.py
+++ bar.py
@@ -13,7 +13,7 @@ df2018=pd.read_csv('./data/Master_2018-19.csv',index_col='SCHOOL_DSTRCT_NM')
 df2018=df2018.iloc[0:180,:]
 plot_types=list(df2018.columns)
 plot_types=plot_types[1:]
-#plot_types.remove('HOPE_ELIGIBLE_PCT')
+
 df2018=df2018.add_suffix('_2018-19')
 
 df2019=pd.read_csv('./data/Master_2019-20.csv',index_col='SCHOOL_DSTRCT_NM').add_suffix('_2019-20')
@@ -30,32 +30,30 @@ for i in plot_types:
                     index=df2018.index)
     width=0.3
     
-    #df.replace('TFS',0.0,inplace=True)
-    #df.str.replace(np.nan)
-    df1=df.iloc[0:59,:]
-    df2=df.iloc[60:119,:]
-    df3=df.iloc[120:,:]
+    
+    df1=df.iloc[0:60,:]
+    df1.name='1st60'
+    df2=df.iloc[61:121,:]
+    df2.name='2nd60'
+    df3=df.iloc[122:,:]
+    df3.name='3rd60'
     
     for k in [df1,df2,df3]:
-        if k==df1: k_str='1st60'
-        if k==df2:k_str='2nd60'
-        if k==df3:k_str='3rd60'
+        k_str=k.name
         labels=list(k.index)
         x=np.arange(len(labels))
         fig,ax=plt.subplots(figsize=(40,15))
         
-        #plt.figure(figsize=(20,20))
+        
         rects1=ax.bar(x-width/3,k.iloc[:,0],width,label='2018-19', color='green')
         rects2=ax.bar(x,k.iloc[:,1],width,label='2019-20', color='blue')
-        rects3=ax.bar(x+width/3,k.iloc[:,2],width,label='2020-21', color='purple')
+        rects3=ax.bar(x+width/3,k.iloc[:,2],width,label='2020-21', color='red')
         ax.legend()
         ax.set_title(i)
-        #ax.figure(figsize=(20,20))
+        ax.tick_params(labelsize=20)
         
-        #ax.bar(x,df,align='center')
         ax.set_xticks(x)
-        # ax.set_ylabel
-        # ax.set_ylabel(np.arange(df_min,df_max))
+        
         ax.set_xticklabels(k.index, rotation=90)
         plt.tight_layout()
         plt.savefig('./images/bar_{}_{}.jpg'.format(i,k_str))
