605157b21068fa0781460ecb6c7532d1a1cb4529
Tala Suidan <talamsuidan@gmail.com>
2023-03-18 16:03:19 -0400

    add values to mpl plots

--- bar.py
+++ bar.py
@@ -7,6 +7,7 @@ Created on Tue Mar 14 10:11:52 2023
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import math
 
 import numpy as np
 
@@ -36,16 +37,22 @@ for i in plot_types:
                      '2020-21':df2020[i+'_2020-21']},\
                     )
         
-    test_df=df.melt(id_vars='District')
+    # test_df=df.melt(id_vars='District')
     
-    p1=sns.FacetGrid(data=test_df, col='District', col_wrap=15,  hue='variable').set_titles( fontsize=18)
-    p1.map(sns.barplot, 'variable', 'value', order=test_df['variable'].unique()).add_legend()#, hue='variable')
-    plt.rcParams['figure.figsize']=(30,24)
-    p1.fig.subplots_adjust(top=0.9)
-    p1.fig.suptitle('{}'.format(i), fontsize=20)
+    # p1=sns.FacetGrid(data=test_df, col='District', col_wrap=15,  hue='variable').set_titles( fontsize=18)
+    # p1.map(sns.barplot, 'variable', 'value', order=test_df['variable'].unique()).add_legend()#, hue='variable')
+    # plt.rcParams['figure.figsize']=(30,24)
+    # p1.fig.subplots_adjust(top=0.9)
+    # p1.fig.suptitle('{}'.format(i), fontsize=20)
     
-    p1.savefig('./images/bar/bar_{}.png'.format(i), dpi=300, bbox_inches='tight')
-    p1.fig.clf()
+    # p1.savefig('./images/bar/bar_{}.png'.format(i), dpi=300, bbox_inches='tight')
+    # p1.fig.clf()
+    y_min=math.floor(np.min(df.iloc[:,1:].min())*.1)
+    
+    y_max=math.ceil(np.max(df.iloc[:,1:].max())*1.1)
+    steps=math.floor((y_max-y_min)/10)
+    if steps==0: steps=1
+    y_ticks_l=list(range(y_min,y_max,steps))
     
     width=0.15    
     df1=df.iloc[0:30,:]
@@ -71,8 +78,32 @@ for i in plot_types:
         
         
         rects1=ax.bar(x-0.2,k.iloc[:,0],width,label='2018-19', color='blue')
+        for p in rects1:
+           height = p.get_height()
+           ax.annotate('{}'.format(height),
+              xy=(p.get_x() + p.get_width() / 2, height),
+              xytext=(0, 3), # 3 points vertical offset
+              textcoords="offset points",
+              ha='center', va='bottom', rotation=90)
+           #ax.set_yticklabels(np.arange(y_min,y_max,(y_max-y_min)/10 ))
         rects2=ax.bar(x,k.iloc[:,1],width,label='2019-20', color='turquoise')
+        for p in rects2:
+           height = p.get_height()
+           ax.annotate('{}'.format(height),
+              xy=(p.get_x() + p.get_width() / 2, height),
+              xytext=(0, 3), # 3 points vertical offset
+              textcoords="offset points",
+              ha='center', va='bottom', rotation=90)
+           #ax.set_yticklabels(np.arange(y_min,y_max,(y_max-y_min)/10 ))
         rects3=ax.bar(x+0.2,k.iloc[:,2],width,label='2020-21', color='red')
+        for p in rects3:
+           height = p.get_height()
+           ax.annotate('{}'.format(height),
+              xy=(p.get_x() + p.get_width() / 2, height),
+              xytext=(0, 3), # 3 points vertical offset
+              textcoords="offset points",
+              ha='center', va='bottom', rotation=90)
+           #ax.set_yticklabels(np.arange(y_min,y_max,(y_max-y_min)/10 ))
         ax.legend(fontsize=16)
         ax.set_title(i, fontsize=20)
         ax.tick_params(labelsize=20)
@@ -80,6 +111,10 @@ for i in plot_types:
         ax.set_xticks(x)
         
         ax.set_xticklabels(k.index, rotation=90)
+        ax.set_yticklabels(y_ticks_l)
+        plt.yticks(y_ticks_l)
+        plt.ylim(y_min,y_max)
+        plt.ylabel('{}'.format(i))
         plt.tight_layout()
         plt.savefig('./images/bar/bar_{}_{}.jpg'.format(i,k_str))
         plt.close()
